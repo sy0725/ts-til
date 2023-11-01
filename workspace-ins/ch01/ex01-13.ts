073b7d2f76b22e5d920de83c31388c8a5467b41b
Kilyong Jeong <uzoolove@gmail.com>
2023-11-01 17:47:01 +0900

    .

--- workspace-ins/ch01/ex01-13.ts
+++ workspace-ins/ch01/ex01-13.ts
@@ -1,5 +1,31 @@
 // index signature, string
 (()=>{
-  
+  interface User {
+    name: string;
+    email: string;
+    age?: number;
+    // phone?: string;
+    // address?: string;
+    // hobby?: string;
+    // ...
+    [etc: string]: string | number | undefined;
+  }
+
+  var kim: User = {
+    name: '김철수',
+    email: 'kim@gmail.com',
+    age: 30,
+    phone: '010...',
+    address: '서울시 ...',
+    hobby: '독서'
+  };
+
+  var lee: User = {
+    name: '이영희',
+    email: 'lee@gmail.com',
+    age: 35
+  };
+
+  console.log(kim, lee);
 })();
 
