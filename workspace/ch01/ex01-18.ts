c96b3c01f33f76d12943d2e8dfb93894a5a98f3e
Jeon SunYong <sunyong0725@naver.com>
2023-11-02 11:31:26 +0900

    1102 변수와 상수, enum

--- workspace/ch01/ex01-18.ts
+++ workspace/ch01/ex01-18.ts
@@ -1,4 +1,14 @@
-(()=>{
-  
-})();
+(() => {
+  function getSchedule(subject: string): string | undefined {
+    if ("JavaScript" === subject) {
+      return `JavaScript 수업은 4일 교육 + 2일 프로젝트 입니다.`;
+    } else if ("TypeScript" === subject) {
+      return `TypeScript 수업은 3일 교육 + 1일 프로젝트 입니다.`;
+    }
+  }
 
+  const jsSchedule = getSchedule("JavaScript");
+  const tsSchedule = getSchedule("TypeScript");
+  console.log(jsSchedule);
+  console.log(tsSchedule);
+})();
--- workspace/ch01/ex01-19-01.ts
+++ workspace/ch01/ex01-19-01.ts
@@ -1,1 +1,29 @@
 // ex01-19.ts 복사
+
+// 상수의 그룹화
+(() => {
+  type ReadOnly = {
+    // readonly JS: string;
+    // readonly TS: string;
+    // readonly REACT: string;
+    readonly [language: string]: string; // 인덱스 시그니처를 사용해도 좋다
+  };
+  const ClassName: ReadOnly = {
+    JS: "JavaScript",
+    TS: "TypeScript",
+    REACT: "React",
+  };
+
+  function getSchedule(subject: string): string | undefined {
+    if (ClassName.JS === subject) {
+      return `JavaScript 수업은 4일 교육 + 2일 프로젝트 입니다.`;
+    } else if (ClassName.TS === subject) {
+      return `TypeScript 수업은 3일 교육 + 1일 프로젝트 입니다.`;
+    }
+  }
+
+  const jsSchedule = getSchedule(ClassName.JS);
+  const tsSchedule = getSchedule(ClassName.TS);
+  console.log(jsSchedule);
+  console.log(tsSchedule);
+})();
