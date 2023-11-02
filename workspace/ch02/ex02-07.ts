d806964b6d6572f123a4f450a1d449197a7e13e4
Jeon SunYong <sunyong0725@naver.com>
2023-11-02 16:26:19 +0900

    1102 제네릭

--- workspace/ch02/ex02-07.ts
+++ workspace/ch02/ex02-07.ts
@@ -1,3 +1,15 @@
-(()=>{
+(() => {
+  function echoString(msg: string): string {
+    return msg;
+  }
 
+  function echoNum(msg: number): number {
+    return msg;
+  }
+
+  const result1: string = echoString("hello");
+  console.log(result1);
+
+  const result2: number = echoNum(3.141592);
+  console.log(result2);
 })();
--- workspace/ch02/ex02-09.ts
+++ workspace/ch02/ex02-09.ts
@@ -1,3 +1,59 @@
-(()=>{
+(() => {
+  interface CityDropdown {
+    value: string;
+    selected: boolean;
+  }
 
+  interface ZipcodeDropdown {
+    value: number;
+    selected: boolean;
+  }
+
+  function createDropdown(list: CityDropdown[]): string {
+    let result = "<select>";
+    list.forEach((item) => {
+      result += `<option ${item.selected ? "selected" : ""}>${
+        item.value
+      }</option>`;
+    });
+    result += "<select/>";
+
+    return result;
+  }
+
+  function createDropdown2(list: ZipcodeDropdown[]): string {
+    let result = "<select>";
+    list.forEach((item) => {
+      result += `<option ${item.selected ? "selected" : ""}>${
+        item.value
+      }</option>`;
+    });
+    result += "<select/>";
+
+    return result;
+  }
+
+  var cityList: CityDropdown[] = [
+    {
+      value: "서울시",
+      selected: false,
+    },
+    {
+      value: "인천광역시",
+      selected: false,
+    },
+    {
+      value: "광주시",
+      selected: false,
+    },
+  ];
+
+  const cityTag = createDropdown(cityList);
+  console.log(cityTag);
+
+  var zipcodeList: ZipcodeDropdown[] = [
+    { value: 12345, selected: false },
+    { value: 34567, selected: false },
+    { value: 56789, selected: true },
+  ];
 })();
