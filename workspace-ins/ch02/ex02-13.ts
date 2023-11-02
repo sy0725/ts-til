a55970d80d28d7c2be4b7a877c7b093549ff4b94
Kilyong Jeong <uzoolove@gmail.com>
2023-11-02 17:29:32 +0900

    .

--- workspace-ins/ch02/ex02-13.ts
+++ workspace-ins/ch02/ex02-13.ts
@@ -1,3 +1,10 @@
 (()=>{
+  function echo<T extends string | number>(msg: T): T{
+    return msg;
+  }
 
+  console.log(echo('hello'));
+  console.log(echo(100));
+  console.log(echo(new Date()));
 })();
+
