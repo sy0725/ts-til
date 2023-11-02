f4825b13a8c5300ca88718af2b715cfd29e61c06
Jeon SunYong <sunyong0725@naver.com>
2023-11-02 16:48:03 +0900

    1102 제네릭 추가

--- workspace/ch02/ex02-09.ts
+++ workspace/ch02/ex02-09.ts
@@ -10,44 +10,34 @@
   }
 
   function createDropdown(list: CityDropdown[]): string {
+    // <select><option>서울시</option><option selected>인천시</option><option>광주시</option></select>
     let result = "<select>";
     list.forEach((item) => {
       result += `<option ${item.selected ? "selected" : ""}>${
         item.value
       }</option>`;
     });
-    result += "<select/>";
-
+    result += "</select>";
     return result;
   }
 
   function createDropdown2(list: ZipcodeDropdown[]): string {
+    // <select><option>서울시</option><option selected>인천시</option><option>광주시</option></select>
     let result = "<select>";
     list.forEach((item) => {
       result += `<option ${item.selected ? "selected" : ""}>${
         item.value
       }</option>`;
     });
-    result += "<select/>";
-
+    result += "</select>";
     return result;
   }
 
   var cityList: CityDropdown[] = [
-    {
-      value: "서울시",
-      selected: false,
-    },
-    {
-      value: "인천광역시",
-      selected: false,
-    },
-    {
-      value: "광주시",
-      selected: false,
-    },
+    { value: "서울시", selected: false },
+    { value: "인천시", selected: true },
+    { value: "광주시", selected: false },
   ];
-
   const cityTag = createDropdown(cityList);
   console.log(cityTag);
 
@@ -56,4 +46,6 @@
     { value: 34567, selected: false },
     { value: 56789, selected: true },
   ];
+  const zipcodeTag = createDropdown2(zipcodeList);
+  console.log(zipcodeTag);
 })();
--- workspace/ch02/ex02-12.ts
+++ workspace/ch02/ex02-12.ts
@@ -1,3 +1,31 @@
-(()=>{
+(() => {
+  class Queue<T> {
+    data: T[] = [];
+    push(item: T): void {
+      this.data.push(item);
+    }
+    pop(): T | undefined {
+      return this.data.shift();
+    }
+  }
 
+  var sq = new Queue();
+  sq.push("hello");
+  sq.push("javascript");
+  sq.push("world");
+
+  var s1 = sq.pop();
+  var s2 = sq.pop();
+  var s3 = sq.pop();
+  console.log(s1, s2, s3);
+
+  var nq = new Queue();
+  nq.push("hello");
+  nq.push("javascript");
+  nq.push("world");
+
+  var n1 = nq.pop();
+  var n2 = nq.pop();
+  var n3 = nq.pop();
+  console.log(n1, n2, n3);
 })();
