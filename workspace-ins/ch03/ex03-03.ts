7d1b2f05514dee6ef4e8302a7da09e10d73a7a52
Kilyong Jeong <uzoolove@gmail.com>
2023-11-03 11:07:04 +0900

    .

--- workspace-ins/ch03/ex03-03.ts
+++ workspace-ins/ch03/ex03-03.ts
@@ -1,3 +1,28 @@
 (()=>{
-  
+  interface Todo {
+    title: string;
+    content: string;
+  }
+  const todo1: Todo = {
+    title: '할일 1',
+    content: '내용 1'
+  };
+  const todo2: Partial<Todo> = {
+    content: '내용 2'
+  };
+  const todo3: Partial<Todo> = {
+    title: '제목 2'
+  };
+  function updateTodo(todo: Todo, newTodo: Partial<Todo>){
+    return { ...todo, ...newTodo };
+  }
+
+  console.log(todo1, todo2, todo3);
+
+  console.log(updateTodo(todo1, todo2));
+  console.log(updateTodo(todo1, todo3));
+
+  todo1.title = '수정된 할일 1';
+
+  console.log(todo1, todo2, todo3);
 })();
