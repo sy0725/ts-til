28acbe1495718c0454cf368e71415dac32908a8b
Kilyong Jeong <uzoolove@gmail.com>
2023-11-03 11:32:48 +0900

    .

--- workspace-ins/ch03/ex03-06.ts
+++ workspace-ins/ch03/ex03-06.ts
@@ -1,3 +1,16 @@
 (()=>{
-  
+  type UserFields = 'id' | 'name' | 'address' | 'phone';
+
+  type User = {
+    [Field in UserFields]: string;
+  };
+
+  const kim: User = {
+    id: 'kim',
+    name: '김철수',
+    address: '경기도',
+    phone: '0102223333'
+  };
+
+  console.log(kim);
 })();
