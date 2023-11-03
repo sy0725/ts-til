732d725e6b36a28fc800c5b7d368592bf8e6978e
Jeon SunYong <sunyong0725@naver.com>
2023-11-03 11:18:55 +0900

    1103 Mapped Type

--- workspace/ch03/ex03-06.ts
+++ workspace/ch03/ex03-06.ts
@@ -1,3 +1,14 @@
-(()=>{
-  
+(() => {
+  type UserFields = "id" | "name" | "address" | "phone";
+
+  type User = {
+    [Field in UserFields]: string; //
+  };
+
+  const kim: User = {
+    id: "kim",
+    name: "김철수",
+    address: "경기도",
+    phone: "0100000000",
+  };
 })();
--- workspace/ch03/ex03-07.ts
+++ workspace/ch03/ex03-07.ts
@@ -1,3 +1,26 @@
-(()=>{
-  
+(() => {
+  type User = {
+    id: string;
+    name: string;
+    address: string;
+    phone: string;
+  };
+
+  type ArrayUser = {
+    [Prop in keyof User]: string[]; // keyof 를 사용하면 User가 가지고 있는 값들을 인해 만들어준다.  string[]로 새로운 ArrayUser 라는 타입 생성
+  };
+
+  const kim: User = {
+    id: "kim",
+    name: "김철수",
+    address: "서울시",
+    phone: "010000000",
+  };
+
+  const lee: ArrayUser = {
+    id: ["kim"],
+    name: ["김철수"],
+    address: ["서울시"],
+    phone: ["010000000"],
+  };
 })();
