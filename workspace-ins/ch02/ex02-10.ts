da9cf38c727fd859cc65e19ac5cf5f8b9ccbffb8
Kilyong Jeong <uzoolove@gmail.com>
2023-11-03 09:20:55 +0900

    .

--- workspace-ins/ch02/ex02-10.ts
+++ workspace-ins/ch02/ex02-10.ts
@@ -4,7 +4,7 @@
     selected: boolean;
   }
 
-  function createDropdown<T>(list: Dropdown<T>[]): string {
+  function createDropdown<T>(list: Dropdown<T>[]) {
     // <select><option>서울시</option><option selected>인천시</option><option>광주시</option></select>
     let result = '<select>';
     list.forEach(item => {
@@ -20,7 +20,7 @@
     { value: '광주시', selected: false },
   ];
   const cityTag = createDropdown<string>(cityList);
-  console.log(cityTag);
+  console.log(cityTag.toLowerCase());
 
   var zipcodeList: Dropdown<number>[] = [
     { value: 12345, selected: false },
