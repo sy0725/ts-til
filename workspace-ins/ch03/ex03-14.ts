4ae8c466a68bcddaa549585836bd46af808f55aa
Kilyong Jeong <uzoolove@gmail.com>
2023-11-03 15:17:14 +0900

    .

--- workspace-ins/ch03/ex03-14.ts
+++ workspace-ins/ch03/ex03-14.ts
@@ -1,3 +1,10 @@
 (()=>{
-  
+  var div = document.querySelector('div');
+  // non null assertion
+  // div!.innerHTML = 'hello';
+
+  console.log(div);
+  if(div !== null){ // 타입 가드
+    div.innerHTML = 'hello';
+  }
 })();
